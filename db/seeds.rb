9145cd22b85c3c178e763ca7876979bf06deb535
ravneetkaur14rrc <ravneetkaur14@rrc.ca>
2025-03-06 21:44:38 -0600

    Updated seed script to load products from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
 require 'faker'
+require 'csv'
 
 Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product["category_name"])
+  category.products.create(
+    title: product["title"],
+    description: product["description"],
+    price: product["price"],
+    stock_quantity: product["stock_quantity"]
+  )
 
 676.times do
   Product.create(
@@ -12,3 +27,4 @@ Product.destroy_all
 end
 
 puts "Created #{Product.count} products."
+puts "Created #{Category.count} categories and #{Product.count} products."
